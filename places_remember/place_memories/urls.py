785392884fb3e234dc13049859fac9137e8d41e1
nvakiet <nvakiet191@gmail.com>
2023-01-17 02:37:52 +0700

    Added simple Hello World page to check if the app run successfully

---
+++ places_remember/place_memories/urls.py
@@ -0,0 +1,14 @@
+"""place_memories web app URL configurations
+
+The urlpatterns route URL to specific pages in the web app.
+"""
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+app_name = 'place_memories'
+
+urlpatterns = [
+    path('', views.home, name='home')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- places_remember/place_memories/views.py
+++ places_remember/place_memories/views.py
@@ -1,3 +1,9 @@
+"""This module contains the views of place_memories web app.
+"""
 from django.shortcuts import render
 
 # Create your views here.
+def home(requests):
+    """Render the homepage of the app.
+    """
+    return render(requests, "place_memories/home.html")
