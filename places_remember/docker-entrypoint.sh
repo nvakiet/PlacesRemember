78990a53b5363499331e6ba4198708fbbf6bb745
Ngo Kiet <nvakiet191@gmail.com>
2023-02-04 13:13:00 +0700

    Change the Nginx config and docker-entrypoint.sh to run with production or development mode
    Since this project only has 1 web app, the Nginx container will replace
    the new configuration template directly at the top level. The entrypoint
    script of the web app container now can be run in development mode using
    Uvicorn or production mode using Gunicorn with Uvicorn workers.

--- places_remember/docker-entrypoint.sh
+++ places_remember/docker-entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Collect static files
-echo "Collect static files"
+echo "Collecting static files"
 python manage.py collectstatic --noinput
 
 # Apply database migrations
-echo "Apply database migrations"
+echo "Applying database migrations"
 python manage.py migrate
 
-# Start server
-echo "Starting server"
-# python manage.py runserver 0.0.0.0:8000
-uvicorn places_remember.asgi:application --proxy-headers --forwarded-allow-ips='*' --reload --host 0.0.0.0
+# Start server in production or development mode
+if [$DEPLOY_MODE == "DEV"]; then
+    echo "Starting server in development mode"
+    uvicorn places_remember.asgi:application --proxy-headers --forwarded-allow-ips='*' --reload --host 0.0.0.0
+else
+    echo "Starting server in production mode"
+    gunicorn -w $N_UVICORN_WORKERS -k uvicorn.workers.UvicornWorker --proxy-protocol --forwarded-allow-ips='*' --bind 0.0.0.0 places_remember.asgi:application
+fi
